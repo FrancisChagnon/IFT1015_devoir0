4d908355e63f6aaa7df7a800dcaf3fc95122a8eb
FrancisChagnon <francis.chagnon@videotron.ca>
2019-01-11 16:17:38 -0500

    Create devoir0.js

---
+++ devoir0.js
@@ -0,0 +1,18 @@
+// Équipier 1 : Marie-Anne Prud'Homme-Maurice (p1054064)
+// Équipier 2 : Francis Chagnon (p0090943)
+
+//Salutation
+var prenom = "Francis";
+var nom = "Chagnon";
+var salutation = "Bonjour, mon nom est " + prenom + " " +  nom + "!";
+
+//Informations
+var age = 47;
+var discipline = "études libres";
+var informations = "J'ai " + age + " ans et j'étudie en " + discipline + "!";
+
+//Introduction et sortie dans la console
+var introduction = salutation + "\n" + informations;
+print(introduction);
+
+//load("corrections.js");
